173c73a63993a7ddc397a70641e050b32a8a485b
santikaragozian <ksantiago@udesa.edu.ar>
2020-06-06 20:03:59 +0200

    top tracks con deezer JS

---
+++ home/index.js
@@ -0,0 +1,70 @@
+window.addEventListener("load", function(){
+    fetch (" https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
+   
+    .then(
+        function(respuesta) {
+            return respuesta.json();            
+        }
+    )
+    
+    .then(
+        function (information) {
+            
+            let topTracks = information.tracks.data
+            console.log(topTracks)
+
+            for (let index = 0; index < topTracks.length; index++) {
+                const cadaTrack = topTracks[index];
+                
+                let title = cadaTrack.title
+                let id = cadaTrack.id
+                let album = cadaTrack.album.cover
+                let artist = cadaTrack.artist.name
+                
+                console.log(title)
+                console.log(id)
+
+                let htmlNuevoTrack = `
+                 
+
+                   
+                    <a href="../detalle/detalle.html?idDeTrack=` + id + `">
+                    <li>     
+                    
+                    <img class="lista-canciones" src="` + album + `">
+
+                    ` + title + ` 
+
+                    <br>
+                    <br>
+                    
+                    
+                    <p class="artista">` + artist + `</p>
+
+                    </li>
+                    </a>
+                    
+                    
+
+                
+                `
+
+                document.querySelector(".artista")
+
+                artist.style.fontStyle = "italic"
+
+                document.querySelector(".lista-canciones").innerHTML += htmlNuevoTrack
+
+
+                console.log(htmlNuevoTrack)
+            }
+        }
+        
+    
+    )
+
+
+
+
+
+})
