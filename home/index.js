6adad1461eccf558db078a0a110e92bbd58161b2
santikaragozian <ksantiago@udesa.edu.ar>
2020-06-09 21:47:27 +0200

    home top albums JS

--- home/index.js
+++ home/index.js
@@ -1,3 +1,4 @@
+/* TOP TRACKS */
 window.addEventListener("load", function(){
     fetch (" https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
    
@@ -61,8 +62,114 @@ window.addEventListener("load", function(){
     
     )
 
-
-
-
-
 })
+
+/* TOP ARTISTS */
+
+/* TOP TRACKS */
+window.addEventListener("load", function(){
+    fetch (" https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
+   
+    .then(
+        function(respuesta) {
+            return respuesta.json();            
+        }
+    )
+    
+    .then(
+        function (information) {
+            
+            let topArtists = information.artists.data
+            console.log(topArtists)
+
+            for (let index = 0; index < topArtists.length; index++) {
+                const cadaArtist = topArtists[index];
+            
+                let id = cadaArtist.id
+                let artist = cadaArtist.name
+                let artistImg = cadaArtist.picture_small
+                
+                console.log(artist)
+                console.log(id)
+
+                let htmlNuevoArtist = `
+
+                <a href="../detalle/detalle.html?idDeTrack=` + id + `">
+                    <li class="topArtistas"> 
+                        <img src="` + artistImg + `" alt="foto the weekend"> 
+                        ` + artist + `
+                    </li>
+                </a>
+                    
+                    
+
+                
+                `
+
+                
+
+                document.querySelector(".topArtistas").innerHTML += htmlNuevoArtist
+
+
+                console.log(htmlNuevoArtist)
+            }
+        }
+    
+    )}
+
+        
+)   
+
+window.addEventListener("load", function(){
+    fetch (" https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
+   
+    .then(
+        function(respuesta) {
+            return respuesta.json();            
+        }
+    )
+    
+    .then(
+        function (information) {
+            
+            let topAlbums = information.albums.data
+            console.log(topAlbums)
+
+            for (let index = 0; index < topArtists.length; index++) {
+                const cadaAlbum = topAlbums[index];
+            
+                let id = cadaAlbum.id
+                let album = cadaAlbum.name
+                let albumImg = cadaAlbum.picture_small
+                
+                console.log(album)
+                console.log(id)
+
+                let htmlNuevoArtist = `
+
+                <a href="../detalle/detalle.html?idDeTrack=` + id + `">
+                    <li class="topArtistas"> 
+                        <img src="` + artistImg + `" alt="foto the weekend"> 
+                        ` + artist + `
+                    </li>
+                </a>
+                    
+                    
+
+                
+                `
+
+                
+
+                document.querySelector(".topArtistas").innerHTML += htmlNuevoArtist
+
+
+                console.log(htmlNuevoArtist)
+            }
+        }
+    
+    )}
+
+        
+)   
+
