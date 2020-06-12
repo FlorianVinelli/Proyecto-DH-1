5255bf6ab1df490450f7f718b0d739eda319a497
unknown <fscaglioni@udesa.edu.ar>
2020-06-12 15:56:47 -0300

    generos terminado

--- detalle/generos.js
+++ detalle/generos.js
@@ -1,18 +1,43 @@
 window.addEventListener ("load", function() {
 
-    const apiRoute = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/"
-    fetch("https://api.deezer.com/genre/0")
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre")
     .then(
         function(respuesta) {
             return respuesta.json();
         }
     )
 
-    .then (
-        function(resultado) {
-            let listaGeneros = resultado.genres_id;
+    .then(
+        function (informacion) {
 
-            console.log(listaGeneros)
+            let todosGeneros = informacion.data
+            console.log(todosGeneros)
+
+            // LISTADO GENEROS //
+
+            for (let index = 0; index < todosGeneros.length; index++) {
+                const cadaGenero = todosGeneros[index];
+
+                let id = cadaGenero.id
+                let name = cadaGenero.name
+                let picture = cadaGenero.picture
+
+                console.log(id)
+                console.log(name)
+
+                let htmlDeGeneros = `
+
+                <a href="../detalle/detalle.html?idDeTrack=` + id + `">
+                <img class="listadoDeGeneros" src= "`+ picture +`">
+
+                <li> `+ name + `</li>
+                
+                </a>`
+
+                document.querySelector(".listadoDeGeneros").innerHTML += htmlDeGeneros
+             
+                console.log(htmlDeGeneros)
+            }
         }
     )
 })
